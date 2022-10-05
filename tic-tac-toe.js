6b03685d684abcb85e4954b07d7c5ec94601ae8e
Alyssa Walker <alyssawalker352@gmail.com>
2022-10-05 14:32:18 -0500

    Ex. 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,5 @@
 
+move1 = 0;
 
 window.onload = loadpage;
 
@@ -10,5 +11,23 @@ function loadpage()
     for(i=0; i<= spaces-1; i++)
     {
         start.children[i].classList.add("square")       
+        start.children[i].addEventListener('click', clicker)
     }
 }
+
+function clicker(event)
+{
+    pos = Array.from(start.children).indexOf(event.target)
+    if (move1 == 0)
+    {
+        start.children[pos].classList.add("O")
+        start.children[pos].innerHTML= "O"
+        move1 = 1
+    } else
+    {
+        start.children[pos].classList.add("X")
+        start.children[pos].innerHTML= "X"
+        move1 = 0
+    }
+
+console.log(pos)}
