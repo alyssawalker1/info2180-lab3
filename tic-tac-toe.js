52bf7b07d40b6efa7fa4a6358e466ed783ac8d6b
Alyssa Walker <alyssawalker352@gmail.com>
2022-10-10 13:07:36 -0500

    Ex. 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,7 @@
-
-move1 = 0;
-
 window.onload = loadpage;
 
 function loadpage()
-{ 
+{   move1 = 0;
     start = document.getElementById("board")
     spaces = start.childElementCount
 
@@ -30,17 +27,76 @@ function highlight(event)
 
 function clicker(event)
 {
+
+
     pos = Array.from(start.children).indexOf(event.target)
     if (move1 == 0)
-    {
+    {   
         start.children[pos].classList.add("O")
         start.children[pos].innerHTML= "O"
         move1 = 1
+        tictactoe.stateofo[pos] = true
     } else
-    {
+    { 
         start.children[pos].classList.add("X")
         start.children[pos].innerHTML= "X"
         move1 = 0
+        tictactoe.stateofx[pos] = true
     }
 
-console.log(pos)}
+    winner()
+
+}
+
+const tictactoe = {
+    turns: true, 
+    stateofx: [false,false,false,false,false,false,false,false,false],
+    stateofo: [false,false,false,false,false,false,false,false,false],
+
+    win_states: [
+        //columns 
+        ['0','3','6'],
+        ['1','4','7'],
+        ['2','5','8'],
+
+        //rows
+        ['0','1','2'],
+        ['3','4','5'],
+        ['6','7','8'],
+
+        //Diagonally
+        ['0','4','8'],
+        ['2','4','6'],
+    ]
+
+}
+
+function winner()
+{   
+    winbool = false
+
+    x_winco = 0
+    o_winco = 0  
+
+    for (count = 0; count<= tictactoe.win_states.length-1; count ++)
+    {
+        for (innercount = 0; innercount <= tictactoe.win_states[count].length-1; innercount ++)
+        { 
+            if(tictactoe.stateofx[tictactoe.win_states[count][innercount]] == true){
+                x_winco ++
+                if(x_winco == 3){
+                    document.getElementById("status").innerHTML = "Congratulations! X is the Winner!"
+                    document.getElementById("status").classList.add("you-won")
+                }
+            }else if(tictactoe.stateofo[tictactoe.win_states[count][innercount]] == true){
+                o_winco ++
+                if(o_winco == 3){
+                    document.getElementById("status").innerHTML = "Congratulations! O is the Winner!"
+                    document.getElementById("status").classList.add("you-won")
+                }
+            }
+        }
+        x_winco = 0
+        o_winco = 0
+    }
+}
